0172aac2b74410771c9383a081d1c5f4c1c35517
Behod Halil <behzoddev@gmail.com>
2022-06-23 23:48:55 +0900

    Add Configurations.kt

---
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -0,0 +1,9 @@
+object Configuration {
+    const val compileSdkVersion = 32
+    const val targetSdkVersion = 32
+    const val applicationId = "uz.behzoddev.habits"
+    const val minSdkVersion = 31
+    const val versionName = "1.0"
+    const val versionCode = 1
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
