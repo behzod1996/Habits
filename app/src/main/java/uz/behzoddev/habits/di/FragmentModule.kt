928d88953173117f9ff22407ced4afafacf40352
Behod Halil <behzoddev@gmail.com>
2022-06-23 23:49:22 +0900

    Add FragmentModule.kt

---
+++ app/src/main/java/uz/behzoddev/habits/di/FragmentModule.kt
@@ -0,0 +1,10 @@
+package uz.behzoddev.habits.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+
+@Module
+@InstallIn(FragmentComponent::class)
+interface FragmentModule {
+}
