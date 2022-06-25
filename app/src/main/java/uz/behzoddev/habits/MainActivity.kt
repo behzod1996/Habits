ae3c7b099939e121497d175ec07cc4d4c43b53df
Behod Halil <behzoddev@gmail.com>
2022-06-26 00:01:28 +0900

    Update MainActivity.kt

--- app/src/main/java/uz/behzoddev/habits/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/habits/MainActivity.kt
@@ -3,12 +3,26 @@ package uz.behzoddev.habits
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import uz.behzoddev.habits.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+
+    }
+
+    private fun setupUI() {
+        binding.apply {
+            binding.bottomNavigation.itemIconTintList = null
+            binding.bottomNavigation.itemIconSize = resources.getDimension(R.dimen.dimen_20dp).toInt()
+        }
     }
 }
