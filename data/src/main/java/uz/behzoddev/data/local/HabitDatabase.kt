c4e628ac733a22e2a5320fbd523e44ffcd8e4978
Behod Halil <behzoddev@gmail.com>
2022-06-25 23:59:54 +0900

    Add HabitDatabase.kt

---
+++ data/src/main/java/uz/behzoddev/data/local/HabitDatabase.kt
@@ -0,0 +1,14 @@
+package uz.behzoddev.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import uz.behzoddev.data.local.entity.Habit
+
+/**
+@Database(
+    entities = [Habit::class],
+    version = 1
+)
+abstract class HabitDatabase : RoomDatabase() {
+
+}*/
