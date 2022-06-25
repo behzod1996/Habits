b4e5026f176278428b2f6962231cdc41e11bb9de
Behod Halil <behzoddev@gmail.com>
2022-06-26 00:01:21 +0900

    Update FragmentModule.kt

--- app/src/main/java/uz/behzoddev/habits/di/FragmentModule.kt
+++ app/src/main/java/uz/behzoddev/habits/di/FragmentModule.kt
@@ -1,10 +1,15 @@
 package uz.behzoddev.habits.di
 
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.FragmentComponent
+import uz.behzoddev.feature_onboarding.OnboardingNavigator
+import uz.behzoddev.habits.OnboardingNavigatorImpl
 
 @Module
 @InstallIn(FragmentComponent::class)
 interface FragmentModule {
+
 }
