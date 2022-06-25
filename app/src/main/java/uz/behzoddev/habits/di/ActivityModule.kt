d644a3f7aa5b001d1b49e518f642183b17cec29e
Behod Halil <behzoddev@gmail.com>
2022-06-25 23:58:53 +0900

    Update ActivityModule.kt

--- app/src/main/java/uz/behzoddev/habits/di/ActivityModule.kt
+++ app/src/main/java/uz/behzoddev/habits/di/ActivityModule.kt
@@ -4,7 +4,9 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import uz.behzoddev.feature_onboarding.OnboardingNavigator
 import uz.behzoddev.feature_splash.SplashNavigator
+import uz.behzoddev.habits.OnboardingNavigatorImpl
 import uz.behzoddev.habits.SplashNavigatorImpl
 
 @Module
@@ -15,4 +17,9 @@ interface ActivityModule {
     fun providesSplashNavigator(
         navigator: SplashNavigatorImpl
     ): SplashNavigator
+
+    @Binds
+    fun providesOnboardingNavigator(
+        navigator: OnboardingNavigatorImpl
+    ): OnboardingNavigator
 }
