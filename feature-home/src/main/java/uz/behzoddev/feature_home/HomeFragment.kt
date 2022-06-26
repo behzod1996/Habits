5f4d5d59966c0423db7acce267adc5f027a5aaa7
Behod Halil <behzoddev@gmail.com>
2022-06-26 21:38:28 +0900

    Update HomeFragment.kt

--- feature-home/src/main/java/uz/behzoddev/feature_home/HomeFragment.kt
+++ feature-home/src/main/java/uz/behzoddev/feature_home/HomeFragment.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.PagerSnapHelper
+import com.tejpratapsingh.recyclercalendar.model.RecyclerCalendarConfiguration
 import dagger.hilt.android.AndroidEntryPoint
 import uz.behzoddev.feature_home.databinding.FragmentHomeBinding
+import java.util.*
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding get() = _binding!!
+    private lateinit var calendarAdapter : HorizontalCalendarAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +29,47 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerConfiguration()
+    }
+
+    private fun setupRecyclerConfiguration() {
+
+        val date = Date()
+        date.time = System.currentTimeMillis()
+
+        val startCalendar = Calendar.getInstance()
+        val endCalendar = Calendar.getInstance()
+
+        endCalendar.time = date
+        endCalendar.add(Calendar.MONTH,1)
+
+        val configuration =
+            RecyclerCalendarConfiguration(
+                calenderViewType = RecyclerCalendarConfiguration.CalenderViewType.HORIZONTAL,
+                calendarLocale = Locale.getDefault(),
+                includeMonthHeader = true
+            )
+
+        configuration.weekStartOffset = RecyclerCalendarConfiguration.START_DAY_OF_WEEK.SUNDAY
+
+        calendarAdapter = HorizontalCalendarAdapter(
+            startDate = startCalendar.time,
+            endDate = endCalendar.time,
+            configuration = configuration,
+            selectedDate = date,
+
+            dateSelectListener = object : HorizontalCalendarAdapter.OnDateSelected {
+                override fun onDateSelected(date: Date) {
+
+                }
+            }
+        )
+
+        binding.rvCalendar.adapter = calendarAdapter
+
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(binding.rvCalendar)
     }
 
     override fun onDestroy() {
